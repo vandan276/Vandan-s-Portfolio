31c0ec625cf6275afabf8210f1ab156674fa544a
vandan276 <113331754+vandan276@users.noreply.github.com>
2024-04-01 15:11:51 +0530

    Vandan's Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function toggleMenu(){
+    const menu = document.querySelector(".menulinks");
+    const icon = document.querySelector(".hi");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+} 
